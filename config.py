ddb114bc6facf680b69d39d925fe39861f2f4a61
ByNiko007 <heisniko007@gmail.com>
2026-07-02 02:58:08 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,13 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-SOURCE_CHANNEL = "kriptokurduhaber"
 CHECK_INTERVAL_MINUTES = 15
+
+RSS_FEEDS = [
+    {
+        "name": "Kripto Kurdu",
+        "url": "https://rsshub.app/telegram/channel/kriptokurduhaber",
+        "category": "🪙 Kripto",
+        "emoji": "🐺"
+    },
+]
